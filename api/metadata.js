18b234769b7a80583f469afe8fa525a6fc03ed41
Chirag Nagendra <chirag.nagendra@gmail.com>
2025-12-22 19:26:21 -0500

    updated js files

--- api/metadata.js
+++ api/metadata.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: process.env.DB_HOST || 'db.qcrgyeosydtcptrantke.supabase.co',
-  port: process.env.DB_PORT || 6543,
+  port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'postgres',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD,
